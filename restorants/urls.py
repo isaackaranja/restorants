12d43ea62394786355e3492f95c4fba170ae1721
EliasKioni <eliaskioni94@gmail.com>
2019-04-27 12:59:33 +0300

    urls

--- restorants/urls.py
+++ restorants/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from hotel.views import HotelViewList, HotelView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hotels/', HotelViewList().as_view()),
+    #path('hotels/<int:pk>/', HotelView().as_view()),
 ]
